acad81ac61679a2a566c731c5ed6a0efc2cdf313
jSnows31 <julio_neves@usp.br>
2024-10-08 15:33:46 -0300

    Update fila.c

--- tad_fila/fila.c
+++ tad_fila/fila.c
@@ -1,25 +1,61 @@
 #include <stdio.h>
 #include "fila.h"
 
-void Cria(Fila* F) {
-  F->inf = NULL;
-  F->sup = NULL;
+void Cria(Fila* F) 
+{
+    F->inf = NULL; 
+    F->sup = NULL;
 }
 
-int Inserir(Fila* F, elem* x) {
-  Node *novo = (Node*) malloc(sizeof(Node));
-  if(novo != NULL) {
-    novo->info = *x;
-    novo->prox = 
-
-    
-    return 0;
-  }
-  return 1;
+int Inserir(Fila* F, elem* x) 
+{
+    No* novo = (No*) malloc(sizeof(Node));
+    if(novo != NULL) 
+    {
+        novo->info = *x;
+        novo->prox = NULL;
+        if(F->inf == NULL)
+            F->inf = novo;
+        else 
+            F->sup->prox = novo;
+        F->sup = novo;
+        return 0;
+    }
+    return 1;
 }
 
-Criar
-Inserir
-Remover
-EstaVazia
-Esvaziar
+int EstaVazia(Fila* F)
+{
+    return (F->inf == NULL? 1 : 0);
+}
+
+int Remover(Fila* F, elem* x)
+{
+    if(!EstaVazia(F)) 
+    {
+        *x = F->info;
+        No* temp = F->inf;
+        F->inf = F->inf->prox;
+        if(F->inf == NULL)
+            F->sup = NULL;
+        free(temp);       
+        return 0;
+    }
+    return 1;
+}
+
+int Esvaziar(Fila* F) 
+{
+    if(!EstaVazia(F))
+    {
+        No* temp = F->inf;
+        while(temp != NULL) 
+        {
+            F->inf = F->inf->prox;
+            free(temp);
+            temp = F->inf;
+        }
+        return 0;
+    }
+    return 1;
+}
