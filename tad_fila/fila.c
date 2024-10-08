f0110c4742fc8d119ebd0ebcafc87ce98ca0d058
jSnows31 <julio_neves@usp.br>
2024-10-08 14:31:39 -0300

    Update fila.c

--- tad_fila/fila.c
+++ tad_fila/fila.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include "fila.h"
 
-Fila* Cria(Fila* F) {
+void Cria(Fila* F) {
   F->inf = NULL;
   F->sup = NULL;
+}
+
+int Inserir(Fila* F, elem* x) {
+  Node *novo = (Node*) malloc(sizeof(Node));
+  if(novo != NULL) {
+    novo->info = *x;
+    novo->prox = 
+
+    
+    return 0;
+  }
+  return 1;
 }
 
 Criar
