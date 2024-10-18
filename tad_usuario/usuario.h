0ec9d78cda0de1794c3833a36e092936798fc4d0
fabio-araujo-silva <fabio.kaue.araujo@gmail.com>
2024-10-18 08:29:36 -0300

    versao inicial header do tad

--- tad_usuario/usuario.h
+++ tad_usuario/usuario.h
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_caracter_nome 50
 #define MAX_caracter_apelido 10
 
-
-typedef struct nousuario{
+typedef struct usuario{
 	char nome[MAX_caracter_nome];
 	char apelido[MAX_caracter_apelido];
-	//outras listas, pilhas e filas do usuario aqui	
-	struct nousuario *prox, *ant;
-} NoUsuario;
+	struct usuario *prox;
+	struct lista_amigos *lista_amigos;
+} Usuario;
 
-typedef struct{
-	NoUsuario *inf, *sup;
-} ListaUsuarios;
+typedef struct amigo{
+	Usuario *conexao;
+	struct amigo *prox;
+} Amigo;
 
-typedef struct{
-	NoAmigo *topo;
-} PilhaAmigos;
+typedef struct lista_amigos{
+	Amigo *inf, *sup;
+} ListaAmigos;
+
+typedef struct lista_usuario{
+	Usuario *inf, *sup;
+} ListaUsuario;
